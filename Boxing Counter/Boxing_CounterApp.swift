fd4e68f78a973ce5fc6daae1014239917999c79e
johnbean393 <johnbean393@gmail.com>
2024-06-17 14:57:49 +0800

    Initial Commit

---
+++ Boxing Counter/Boxing_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boxing_CounterApp.swift
+//  Boxing Counter
+//
+//  Created by Bean John on 17/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Boxing_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
